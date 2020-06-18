f6e577abd0a0d968e70c7c4f555cad902084bc70
lampawood <lampawood7703@gmail.com>
2020-06-18 22:28:01 +0300

    bug Fix

--- script/index.js
+++ script/index.js
@@ -1,32 +1,32 @@
 const proFile = document.querySelector('.profile')
 const popUp = document.querySelector('.pop-up')
-const editButton = proFile.querySelector('.Edit')
+const editButton = proFile.querySelector('.edit')
 const closePop = popUp.querySelector('.pop-up__close')
 const popSave = popUp.querySelector('.pop-up__save')
 
-const openPop = function() {
+const togglePop = function() {
   popUp.classList.toggle('pop-up-open');
 }
 
 const saveButt = function(ev) {
   ev.preventDefault()
   let popName = popUp.querySelector('.pop-up__input_title').value
-  let popJob = popUp.querySelector('.popup__input_subtitle').value
+  let popJob = popUp.querySelector('.pop-up__input_subtitle').value
   let avaName = proFile.querySelector('.profile__title')
   let avaJob = proFile.querySelector('.profile__subtitle')
   avaJob.textContent = popJob;
   avaName.textContent = popName;
-  openPop();
+  togglePop();
 }
 
 const closeOverlay = function(ev) {
   if (ev.target !== ev.currentTarget) {
     return
   }
-  openPop();
+  togglePop();
 }
 
-editButton.addEventListener("click", openPop);
-closePop.addEventListener("click", openPop);
+editButton.addEventListener("click", togglePop);
+closePop.addEventListener("click", togglePop);
 popSave.addEventListener("click", saveButt);
 popUp.addEventListener('click', closeOverlay);
