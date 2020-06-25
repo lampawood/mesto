39707f983a4a4eb818643ab6aa6c27841a5d3db2
lampawood <lampawood7703@gmail.com>
2020-06-25 13:27:17 +0300

    endFix

--- script/index.js
+++ script/index.js
@@ -5,15 +5,15 @@ const popUp = document.querySelector('.pop-up')
 const formElement = popUp.querySelector('.pop-up__form')
 const popOpened = profile.querySelector('.profile__edit')
 const popClose = popUp.querySelector('.pop-up__close')
-const nameInput = profile.querySelector('.profile__title')
-const jobInput = profile.querySelector('.profile__subtitle')
-const popName = popUp.querySelector('.pop-up__input_type_title')
-const popJob = popUp.querySelector('.pop-up__input_type_subtitle')
+const popName = profile.querySelector('.profile__title')
+const popJob = profile.querySelector('.profile__subtitle')
+const nameInput = popUp.querySelector('.pop-up__input_type_title')
+const jobInput = popUp.querySelector('.pop-up__input_type_subtitle')
   
-const popUpToggle = function (ev) {
+const popUpToggle = function () {
     if (!popUp.classList.contains('pop-up__opened')) {
-        popName.value = nameInput.textContent;
-           popJob.value = jobInput.textContent;
+        nameInput.value = popName.textContent;
+        jobInput.value = popJob.textContent;
        }
     popUp.classList.toggle('pop-up__opened')
 }
@@ -25,20 +25,15 @@ function formSubmitHandler (evt) {
                         // Так мы можем определить свою логику отправки.
                         // О том, как это делать, расскажем позже.
     
-    popJob.textContent = popJob;
-    popName.textContent = popName;
+    popJob.textContent = jobInput.value;
+    popName.textContent = nameInput.value;
+    popUpToggle();
       }
       
-const closeOverlay = function(ev) {
-     if (ev.target !== ev.currentTarget) {
-       return
-     }
-        popUpToggle()
-      }
+
       
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler)
 popOpened.addEventListener('click', popUpToggle)
 popClose.addEventListener('click', popUpToggle)
-popUp.addEventListener('click', closeOverlay)
