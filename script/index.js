1a9710922f4376f016354ca64390b01b8015c13d
lampawood <lampawood7703@gmail.com>
2020-06-25 14:43:35 +0300

    last version

--- script/index.js
+++ script/index.js
@@ -1,21 +1,21 @@
 
 // Находим форму в DOM
-const profile = document.querySelector('.profile')
-const popUp = document.querySelector('.pop-up')
-const formElement = popUp.querySelector('.pop-up__form')
-const popOpened = profile.querySelector('.profile__edit')
-const popClose = popUp.querySelector('.pop-up__close')
-const popName = profile.querySelector('.profile__title')
-const popJob = profile.querySelector('.profile__subtitle')
-const nameInput = popUp.querySelector('.pop-up__input_type_title')
-const jobInput = popUp.querySelector('.pop-up__input_type_subtitle')
+const profile = document.querySelector('.profile');
+const popUp = document.querySelector('.pop-up');
+const formElement = popUp.querySelector('.pop-up__form');
+const popOpened = profile.querySelector('.profile__edit');
+const popClose = popUp.querySelector('.pop-up__close');
+const popName = profile.querySelector('.profile__title');
+const popJob = profile.querySelector('.profile__subtitle');
+const nameInput = popUp.querySelector('.pop-up__input_type_title');
+const jobInput = popUp.querySelector('.pop-up__input_type_subtitle');
   
 const popUpToggle = function () {
     if (!popUp.classList.contains('pop-up__opened')) {
         nameInput.value = popName.textContent;
         jobInput.value = popJob.textContent;
        }
-    popUp.classList.toggle('pop-up__opened')
+    popUp.classList.toggle('pop-up__opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -34,6 +34,6 @@ function formSubmitHandler (evt) {
       
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler)
-popOpened.addEventListener('click', popUpToggle)
-popClose.addEventListener('click', popUpToggle)
+formElement.addEventListener('submit', formSubmitHandler);
+popOpened.addEventListener('click', popUpToggle);
+popClose.addEventListener('click', popUpToggle);
