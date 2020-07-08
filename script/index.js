08147e4516ef83e438cb9b977fe62a3409c06755
lampawood <lampawood7703@gmail.com>
2020-07-08 20:33:12 +0300

    End

--- script/index.js
+++ script/index.js
@@ -1,39 +1,205 @@
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+// popup edit profile
+const popupEdit = document.querySelector('.pop-up_type_user');
+const popupEditForm = document.querySelector('.pop-up_type_user_form');
+const popupEditName = document.querySelector('.pop-up__input_type_name');
+const popupEditCaption = document.querySelector('.pop-up__input_type_about');
+const popupEditBtnSave = document.querySelector('.pop-up__save_user');
+const popupEditBtnExit = document.querySelector('.pop-up__close_user');
 
-// Находим форму в DOM
-const profile = document.querySelector('.profile');
-const popUp = document.querySelector('.pop-up');
-const formElement = popUp.querySelector('.pop-up__form');
-const popOpened = profile.querySelector('.profile__edit');
-const popClose = popUp.querySelector('.pop-up__close');
-const popName = profile.querySelector('.profile__title');
-const popJob = profile.querySelector('.profile__subtitle');
-const nameInput = popUp.querySelector('.pop-up__input_type_title');
-const jobInput = popUp.querySelector('.pop-up__input_type_subtitle');
-  
-const popUpToggle = function () {
-    if (!popUp.classList.contains('pop-up__opened')) {
-        nameInput.value = popName.textContent;
-        jobInput.value = popJob.textContent;
-       }
-    popUp.classList.toggle('pop-up__opened');
+// profile 
+const nameProfile = document.querySelector('.profile__title');
+const captionProfile = document.querySelector('.profile__subtitle');
+const buttonEdit = document.querySelector('.profile__edit');
+const creatNewCardBtn = document.querySelector('.profile__add');
+
+// доступ к template content
+const card = document.querySelector('#card').content;
+const gallery = document.querySelector('.elements');
+
+// popup open full image
+const popupFull = document.querySelector('.pop-up-full-image');
+const popupFullImage = document.querySelector('.pop-up-full-image__image');
+const popupFullTitle = document.querySelector('.pop-up-full-image__title');
+const popupFullBtnExit = document.querySelector('.pop-up-full-image__close');
+
+// popup добавления новых карточек на страницу
+const popupAdd = document.querySelector('.pop-up_type_card');
+// форма добавления новых карточек на страницу
+const popupAddForm = document.querySelector('.pop-up_type_card_form');
+// inputs формы
+const popupAddTitle = document.querySelector('.pop-up__card_title');
+const popupAddSrc = document.querySelector('.pop-up__card_link');
+// закрыть btn
+const popupAddBtnExit = document.querySelector('.pop-up__close_card');
+
+// function toggle every popups
+const togglePopup = (popup) => {
+    popup.classList.toggle(`pop-up__opened`);
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-    
-    popJob.textContent = jobInput.value;
-    popName.textContent = nameInput.value;
-    popUpToggle();
-      }
-      
+// fuction изменение профиля
+const editProfile = () => {
+    popupEditName.value = nameProfile.textContent;
+    popupEditCaption.value = captionProfile.textContent;
+}
 
-      
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-popOpened.addEventListener('click', popUpToggle);
-popClose.addEventListener('click', popUpToggle);
+// fuction сохранение изменений в профиле
+const saveChangesPopupEdit = (evt) => {
+    evt.preventDefault();
+    nameProfile.textContent = popupEditName.value;
+    captionProfile.textContent = popupEditCaption.value;
+    togglePopup(popupEdit);
+    editProfile();
+}
+
+// закрытие попапы по нажатию на overlay
+const closePopup = (evt, popup) => {
+    if (evt.target === popup) {
+        togglePopup(popup);
+    }
+}
+
+// функция добавления карточек в конец
+const addCardtoEnd = (card) => {
+    gallery.append(card);
+}
+
+// функция добавления карточки в начало
+const addCardtoStart = (card) => {
+    gallery.prepend(card);
+}
+
+// функция создания карточки
+const createCard = (element) => {
+    const elementCard = card.cloneNode(true);
+    elementCard.querySelector('.card__item').src = element.link;
+    elementCard.querySelector('.card__item').alt = element.name;
+    elementCard.querySelector('.card__title').textContent = element.name;
+    return elementCard;
+}
+
+// функция добавления начальных 6 карточек на страницу
+const createStartersCard = () => {
+    initialCards.forEach((initialCard) => {
+        addCardtoEnd(createCard(initialCard));
+    })
+}
+
+// сохранение новой карточки
+const saveNewCard = (evt) => {
+    evt.preventDefault();
+    const newCard = {
+        name: popupAddTitle.value,
+        link: popupAddSrc.value
+    }
+    addCardtoStart(createCard(newCard));
+    popupAddForm.reset();
+}
+
+// функция переключатель лайков
+const likeToggle = (element) => {
+    element.classList.toggle('card__like_liked')
+}
+
+//функция удаления карточке работа с кнопкой удаления
+const trashButton = (element) => {
+    const listItem = element.closest('.card');
+    listItem.remove();
+}
+
+// функция открытия на весь экран картинки полноразмерной
+const openPopupFull = (element) => {
+    const parentElement = element.closest('.card');
+    const elementTitle = parentElement.querySelector('.card__title');
+    popupFullImage.src = element.src;
+    popupFullImage.alt = elementTitle.textContent;
+    popupFullTitle.textContent = elementTitle.textContent;
+    togglePopup(popupFull);
+}
+
+// вызовы
+createStartersCard();
+
+/// добавление слушателей
+// popup изменение профиля
+buttonEdit.addEventListener('click', () => {
+    togglePopup(popupEdit);
+    editProfile();
+});
+popupEditBtnExit.addEventListener('click', () => {
+    togglePopup(popupEdit);
+});
+popupEditForm.addEventListener('submit', saveChangesPopupEdit);
+
+popupEdit.addEventListener('click', (evt) => {
+    closePopup(evt, popupEdit);
+});
+
+window.addEventListener('keydown', (evt) => {
+    closePopup(evt, popupEdit);
+});
+// слушатель кнопка добавления карточки
+// слушатели для попап добавления новой карточки
+creatNewCardBtn.addEventListener('click', () => {
+    togglePopup(popupAdd);
+});
+popupAddBtnExit.addEventListener('click', () => {
+    togglePopup(popupAdd);
+});
+popupAddForm.addEventListener('submit', (evt) => {
+    saveNewCard(evt);
+    togglePopup(popupAdd);
+});
+popupAdd.addEventListener('click', (evt) => {
+    closePopup(evt, popupAdd);
+});
+window.addEventListener('keydown', (evt) => {
+    closePopup(evt, popupAdd);
+});
+
+// слушатель для попапа full
+popupFullBtnExit.addEventListener('click', () => {
+    togglePopup(popupFull);
+});
+popupFull.addEventListener('click', (evt) => {
+    closePopup(evt, popupFull);
+});
+window.addEventListener('keydown', (evt) => {
+    closePopup(evt, popupFull)
+});
+
+// слушатель галлереи для работы с кнопками (like, trash) и открытия картинки на весь экран
+gallery.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('card__like')) {
+        likeToggle(evt.target);
+    } else if (evt.target.classList.contains('card__delete')) {
+        trashButton(evt.target)
+    } else if (evt.target.classList.contains('card__item')) {
+        openPopupFull(evt.target);
+    }
+})
